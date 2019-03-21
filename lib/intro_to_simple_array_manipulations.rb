e0fd60c47132b0ee3ac997154da6c94e9209a52e
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-21 18:44:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,16 @@ def using_push(array, string)
   array =[]
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array =[]
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+array.pop(2)
+end 
