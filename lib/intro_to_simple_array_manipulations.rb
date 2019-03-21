f7d2f341437113507973bf664c4ed9967a33ff09
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-21 18:51:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,15 @@ end
 def pop_with_args(array)
 array.pop(2)
 end 
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
